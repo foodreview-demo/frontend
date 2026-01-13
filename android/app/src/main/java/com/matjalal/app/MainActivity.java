ea6372e2efe6f5e7b45072a2194dd1a1dcbe91b7
son <cliff615@naver.com>
2026-01-13 21:14:46 +0900

    알림 채널 생성 추가

--- android/app/src/main/java/com/matjalal/app/MainActivity.java
+++ android/app/src/main/java/com/matjalal/app/MainActivity.java
@@ -1,5 +1,33 @@
 package com.matjalal.app;
 
+import android.os.Build;
+import android.os.Bundle;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        createNotificationChannel();
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                "default",
+                "기본 알림",
+                NotificationManager.IMPORTANCE_HIGH
+            );
+            channel.setDescription("맛잘알 푸시 알림");
+            channel.enableVibration(true);
+            
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            if (manager != null) {
+                manager.createNotificationChannel(channel);
+            }
+        }
+    }
+}

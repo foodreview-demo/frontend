bd95f30de9e9e8d6c4f920e7ab0a7a14392832c3
son <cliff615@naver.com>
2026-01-13 21:36:25 +0900

    디버깅 로그 추가

--- android/app/src/main/java/com/matjalal/app/MainActivity.java
+++ android/app/src/main/java/com/matjalal/app/MainActivity.java
@@ -1,17 +1,51 @@
 package com.matjalal.app;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.util.Log;
 
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate called");
         createNotificationChannel();
+        handleIntent(getIntent());
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Log.d(TAG, "onNewIntent called");
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        if (intent == null) {
+            Log.d(TAG, "Intent is null");
+            return;
+        }
+
+        Log.d(TAG, "Intent action: " + intent.getAction());
+        Log.d(TAG, "Intent data: " + intent.getDataString());
+        
+        Bundle extras = intent.getExtras();
+        if (extras != null) {
+            Log.d(TAG, "Intent extras:");
+            for (String key : extras.keySet()) {
+                Object value = extras.get(key);
+                Log.d(TAG, "  " + key + " = " + (value != null ? value.toString() : "null"));
+            }
+        } else {
+            Log.d(TAG, "No extras in intent");
+        }
     }
 
     private void createNotificationChannel() {
@@ -27,6 +61,7 @@ public class MainActivity extends BridgeActivity {
             NotificationManager manager = getSystemService(NotificationManager.class);
             if (manager != null) {
                 manager.createNotificationChannel(channel);
+                Log.d(TAG, "Notification channel created");
             }
         }
     }
